ce93ac81f90cc4e8c80d5833a91f7ef6e76138c2
Conrad THIOUNN <thiounn.conrad@gmail.com>
2018-12-18 23:14:52 +0100

    day1 with timing

--- day1.jl
+++ day1.jl
@@ -1,25 +1,29 @@
 #!/usr/bin/env julia
-fname="input\\day1-input.file"
-outputPart1=0
-loop=0
-breakLoop=0
-seen = Int64[]
-while breakLoop==0
-	open(fname) do file
-		for line in eachline(file)
-			#println(line)
-			global outputPart1 += parse(Int32,line)
-			if outputPart1 in seen
-				global breakLoop=1
-				break
-			end
-			push!(seen,outputPart1)
-		end
-		if loop==0
-			println(outputPart1)
-		end
-		global loop+=1
-	end
-end
 
-println(outputPart1)
+function day1()
+	fname="input\\day1-input.file"
+	outputPart1=0
+	loop=0
+	breakLoop=0
+	seen = Int64[]
+	while breakLoop==0
+		open(fname) do file
+			for line in eachline(file)
+				#println(line)
+				global outputPart1 += parse(Int32,line)
+				if outputPart1 in seen
+					global breakLoop=1
+					break
+				end
+				push!(seen,outputPart1)
+			end
+			if loop==0
+				println(outputPart1)
+			end
+			global loop+=1
+		end
+	end
+	
+	println(outputPart1)
+end
+@time day1()
